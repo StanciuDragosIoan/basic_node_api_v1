ccb21b428954092148482b81cba62aef66673ad3
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-09-06 23:40:55 +0300

    re-wrote the howTo.html and changed some stuff in the API code

--- README.md
+++
@@ -1,6 +0,0 @@
-This is a series of blog posts in which I will work on a node API, and futher develop it.
-
-Download the repo and read the howTo.html file if you plan on re-building the project as it is.
-
-*the howTo.html is currently out of date, I am in the process of re-writing it, but the code in /project is the latest
-
--- project/server.js
+++ project/server.js
@@ -1,7 +1,5 @@
 //import http module for our server
 const http = require("http");
-//import file system module
-const fs = require("fs");
 const resource = require("./resource");
 
 //define port for our server to listen to
@@ -11,7 +9,7 @@ const PORT = process.env.port || 5000;
 const server = http.createServer((req, res) => {
   //define url
   const url = req.url;
-  //define method
+  //define request
   const method = req.method;
   //set content type header
   res.setHeader("Content-Type", "text/html");
@@ -20,12 +18,118 @@ const server = http.createServer((req, res) => {
     case "/":
       //write client response
       res.write(`
-        <h1>Welcome to our nodeJS API</h1>
-        <p>This is your first endpoint =)</p>
-            `);
+<h2 
+  style="font-size: 2rem; 
+  text-align:center; 
+  display:block; 
+  margin:auto; 
+  width:30rem; 
+  margin-top:5rem; 
+  border:5px solid #ccc; 
+  border-radius:10px; 
+  padding:3rem;">
+  Welcome to our API   
+</h2>
+<h2 
+  style="font-size: 2rem; 
+  text-align:center; 
+  display:block; 
+  margin:auto; 
+  width:30rem; 
+  margin-top:5rem; 
+  border:5px solid #ccc; 
+  border-radius:10px; 
+  padding:3rem;">
+  Go to to <a 
+            href="http://localhost:5000/resources" 
+            target="_blank">
+              http://localhost:5000/resources
+          </a>  
+          to view all current
+  resources in a pretty format
+</h2>
+<h2 
+  style="font-size: 2rem; 
+  text-align:center; 
+  display:block; 
+  margin:auto; 
+  width:30rem; 
+  margin-top:5rem; 
+  border:5px solid #ccc; 
+  border-radius:10px; 
+  padding:3rem;"
+>
+  Go to to <a 
+            href="http://localhost:5000/resources/json" 
+            target="_blank">
+              http://localhost:5000/resources/json
+            </a>  to view all current
+  resources in a JSON-like format
+</h2>
+
+<h2 
+  style="font-size: 2rem; 
+  text-align:center; 
+  display:block; 
+  margin:auto; 
+  width:30rem; 
+  margin-top:5rem; 
+  border:5px solid #ccc; 
+  border-radius:10px; padding:3rem;"
+>
+  Go to to <a 
+            href="http://localhost:5000/resource/add" 
+            target="_blank">
+              http://localhost:5000/resource/add
+            </a>  to add
+  a new resource. Be careful! The ID you submit must 
+  not already exist, or the API won't let you add 
+  the resource.
+</h2>
+
+<h2 
+  style="font-size: 2rem; 
+  text-align:center; 
+  display:block; 
+  margin:auto; 
+  width:30rem; 
+  margin-top:5rem; 
+  border:5px solid #ccc; 
+  border-radius:10px; 
+  padding:3rem;">
+  Go to to <a 
+      href="#">http://localhost:5000/resource/:id
+    </a>  (e.g. <a 
+          href="#">http://localhost:5000/resource/5
+      </a> to access the resource with id 5)
+  to edit or delete a specific resource. Be careful! 
+  The id you 
+  access via the url must exist.
+</h2>
+
+<h2 
+  style="font-size: 2rem; 
+        text-align:center; 
+        display:block; 
+        margin:auto; 
+        width:30rem; 
+        margin-top:5rem; 
+        border:5px solid #ccc; 
+        border-radius:10px; 
+        padding:3rem;"
+      >
+  Your 'database' is <a href="#">db.json</a>
+  that's where all your resources 
+  data is being persisted so the API 'remembers' it.
+</h2>
+`);
       //send client response
       res.end();
       break;
+    case "/resources/json":
+      resource.getJson(res);
+      break;
+
     case "/resources":
       resource.getAll(res);
       break;
@@ -48,42 +152,58 @@ const server = http.createServer((req, res) => {
           padding:2rem!important; 
           font-size:1.5rem!important;" 
           
-          action="/resource/add"
+          action="/resource/add" 
           
-          method="POST"
-        >
+          method="POST"  
+          >
           <label for="text">Resource Text</label>
           <input name="text" type="text"/>
-          <br> <br> 
+          <br> <sbr> 
           <label for="id">Resource id</label>
           <input name="id" type="number"/>
-          <br> <br>
+          <br> <sbr>
           <label for="date">Submission Date</label>
           <input name="date" type="date"/>
-          <br> <br>
+          <br> <sbr>
           <input 
             style="display:block; 
             margin:auto!important; 
             width:20rem; 
             font-size:2rem;" 
+            
             type="submit" 
             
-            value="Add Resource"> 
+            value="Add Resource"
+          > 
         </form>
         `);
         res.end();
       } else if (method === "POST") {
         resource.addResource(req, res);
       }
-
+      break;
+    case "/resource/edit":
+      if (method === "POST") {
+        resource.editResource(req, res);
+      } else if (method === "GET") {
+        res.write("404 error page not found!");
+        res.end();
+      }
+      break;
+    case "/resource/delete":
+      resource.deleteResource(req, res);
+      break;
+    default:
+      resource.searchEditOrDeleteResource(req, res);
       break;
   }
 });
 
 //log some output to see everything's ok
 console.log(`
-    Server is running on port: 
-    ${PORT} so our API is alive =)`);
+  Server is running on port: 
+  ${PORT} so our API is alive =)
+`);
 
 //start the server
 server.listen(PORT);
